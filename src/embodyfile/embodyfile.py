3a2f39d517afe6f50fb6069d8156fe4994ca27d8
Espen Westgaard <espenwest@gmail.com>
2022-11-13 18:28:41 +0100

    WIP initialise project

--- src/embodyfile/embodyfile.py
+++ src/embodyfile/embodyfile.py
@@ -1,6 +1,7 @@
 """Embody file module to parse binary embody files to HDF, CSV or other output formats."""
 import logging
 
+
 if __name__ == "__main__":
     """Main method for demo and testing"""
     logging.basicConfig(
--- tests/test_embodyfile.py
+++ tests/test_embodyfile.py
@@ -1,5 +1,6 @@
 """Test cases for the embodyfile module."""
 
+
 def test_dummy() -> None:
     """Test dummy."""
     pass
